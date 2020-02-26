471712e915d16c13aa7db3a4c7593e6f640fa100
Callie Glade <callieglade@gmail.com>
2020-02-25 21:19:29 -0800

    Set methods as Array methods and change tabs from 4 to 2

--- enumerables.rb
+++ enumerables.rb
@@ -1,58 +1,72 @@
 module Enumerable
+  class Array
     def my_each(array)
-        i = 0
-        while i < array.length
-            yield(array[i])
-            i += 1
-        end
-        return array
+      i = 0
+      while i < array.length
+          yield(array[i])
+          i += 1
+      end
+      return array
     end
 
     def my_each_with_index(array)
-        i = 0
-        while i < array.length
-            yield(array[i], i)
-            i += 1
-        end
-        return array
+      i = 0
+      while i < array.length
+        yield(array[i], i)
+        i += 1
+      end
+      return array
     end
 
     def my_select(array)
-        new_array = []
-        array.my_each {|item| new_array << item if yield(item)}
-        return new_array
+      new_array = []
+      array.my_each {|item| new_array << item if yield(item)}
+      return new_array
     end
 
     def my_all?(array)
-        array.my_each do |item|
-            if yield(item) == false
-                return false
-            else
-                next
-            end
+      array.my_each do |item|
+        if yield(item) == false
+          return false
+        else
+          next
         end
-        return true
+      end
+      return true
     end
 
     def my_any?(array)
-        array.my_each do |item|
-            if yield(item) == true
-                return true
-            else
-                next
-            end
+      array.my_each do |item|
+        if yield(item) == true
+          return true
+        else
+          next
         end
-        return false
+      end
+      return false
     end
 
     def my_none?(array)
-        array.my_each do |item|
-            if yield(item) == true
-                return false
-            else
-                next
-            end
+      array.my_each do |item|
+        if yield(item) == true
+          return false
+        else
+          next
         end
-        return true
+      end
+      return true
     end
+
+    def my_count(array)
+      count = 0
+      array.my_each {|item| count += 1}
+      return count
+    end
+
+    def my_map(array)
+      new_array = []
+      array.my_each {|item| new_array << yield(item)}
+      return new_array
+    end
+  end
 end
