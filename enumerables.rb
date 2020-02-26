6036ad548fe4c18612203698df1a2ebf4f3f1fff
Callie Glade <callieglade@gmail.com>
2020-02-25 21:43:46 -0800

    Replace 'array' with 'self' in Array methods

--- enumerables.rb
+++ enumerables.rb
@@ -2,30 +2,30 @@ module Enumerable
   class Array
     def my_each 
       i = 0
-      while i < array.length
-          yield(array[i])
+      while i < self.length
+          yield(self[i])
           i += 1
       end
-      return array
+      return self
     end
 
     def my_each_with_index 
       i = 0
-      while i < array.length
-        yield(array[i], i)
+      while i < self.length
+        yield(self[i], i)
         i += 1
       end
-      return array
+      return self
     end
 
     def my_select 
       new_array = []
-      array.my_each {|item| new_array << item if yield(item)}
+      self.my_each {|item| new_array << item if yield(item)}
       return new_array
     end
 
     def my_all? 
-      array.my_each do |item|
+      self.my_each do |item|
         if yield(item) == false
           return false
         else
@@ -36,7 +36,7 @@ module Enumerable
     end
 
     def my_any? 
-      array.my_each do |item|
+      self.my_each do |item|
         if yield(item) == true
           return true
         else
@@ -47,7 +47,7 @@ module Enumerable
     end
 
     def my_none? 
-      array.my_each do |item|
+      self.my_each do |item|
         if yield(item) == true
           return false
         else
@@ -59,13 +59,13 @@ module Enumerable
 
     def my_count 
       count = 0
-      array.my_each {|item| count += 1}
+      self.my_each {|item| count += 1}
       return count
     end
 
     def my_map 
       new_array = []
-      array.my_each {|item| new_array << yield(item)}
+      self.my_each {|item| new_array << yield(item)}
       return new_array
     end
   end
